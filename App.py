9e3045bfdca5454be472636d297c534646ab3264
YangShengzhou <3555844679@qq.com>
2025-04-23 20:19:39 +0800

    枫叶相册媒体管理系统

--- App.py
+++ App.py
@@ -1,49 +1,46 @@
 import sys
-
 from PyQt6 import QtWidgets, QtCore
 from PyQt6.QtNetwork import QLocalSocket, QLocalServer
-
+from contextlib import closing
 from MainWindow import MainWindow
-
-"""
-pyinstaller App.spec
-"""
 
 
 def bring_existing_to_front():
-    socket = QLocalSocket()
-    socket.connectToServer("LeafView_Server")
-    if socket.waitForConnected(500):
-        socket.write(b'bringToFront')
-        socket.waitForBytesWritten(1000)
-        socket.disconnectFromServer()
-        socket.waitForDisconnected(1000)
+    try:
+        with closing(QLocalSocket()) as socket:
+            socket.connectToServer("LeafView_Server")
+            if socket.waitForConnected(500):
+                socket.write(b'bringToFront')
+                socket.waitForBytesWritten(1000)
+    except Exception:
+        pass
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     shared_memory = QtCore.QSharedMemory("LeafView_SharedMemory")
-
     if shared_memory.attach():
         bring_existing_to_front()
         sys.exit()
-
     if not shared_memory.create(1):
         sys.exit()
-
     local_server = QLocalServer()
-    if local_server.listen("LeafView_Server"):
-        def new_connection():
-            socket = local_server.nextPendingConnection()
-            if socket.waitForReadyRead(1000):
-                data = socket.readAll().data()
-                if data == b'bringToFront':
-                    window.activateWindow()
-                    window.raise_()
-                    window.showNormal()
+    if not local_server.listen("LeafView_Server"):
+        sys.exit()
 
-        local_server.newConnection.connect(new_connection)
+    def new_connection():
+        socket = local_server.nextPendingConnection()
+        socket.readyRead.connect(lambda: handle_ready_read(socket))
 
+    def handle_ready_read(socket):
+        data = socket.readAll().data()
+        if data == b'bringToFront':
+            window.activateWindow()
+            window.raise_()
+            window.showNormal()
+        socket.deleteLater()
+
+    local_server.newConnection.connect(new_connection)
     window = MainWindow()
     window.move(300, 100)
     window.show()
